4eac4de8538b30abcea8b64b4013e526c4164bc6
thusy <12thusy1999@gmail.com>
2025-03-01 21:55:00 +0530

    Get employee details using id

--- ems-backend/src/main/java/com/example/ems_backend/service/EmployeeService.java
+++ ems-backend/src/main/java/com/example/ems_backend/service/EmployeeService.java
@@ -4,4 +4,6 @@ import com.example.ems_backend.dto.EmployeeDto;
 
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
 }
